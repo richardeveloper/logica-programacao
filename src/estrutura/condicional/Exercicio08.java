7414ee1c21c089aacbee536c4752df1bbf4bed9f
Richard Anderson Rodrigues <richard_ar@hotmail.com>
2021-02-25 20:59:59 -0300

    Update Exercicio08.java

--- src/estrutura/condicional/Exercicio08.java
+++ src/estrutura/condicional/Exercicio08.java
@@ -1,8 +1,8 @@
-//08. FaÁa um programa para calcular e mostrar o sal·rio reajustado de um funcion·rio. O percentual de aumento encontra-se na tabela a seguir.
+//08. Fa√ßa um programa para calcular e mostrar o sal√°rio reajustado de um funcion√°rio. O percentual de aumento encontra-se na tabela a seguir.
 //
-//	  Sal·rio				Percentual de Aumento
-//    AtÈ R$ 300,00 		35%
-//	  Acima de R$ 300,00 	15%
+//    Sal√°rio				Percentual de Aumento
+//    At√© R$ 300,00 			35%
+//    Acima de R$ 300,00 	        15%
 
 package estrutura.condicional;
 
@@ -14,14 +14,14 @@ public class Exercicio08 {
 		
 		double salario;
 		
-		System.out.println("Digite o sal·rio do funcion·rio: ");
+		System.out.println("Digite o sal√°rio do funcion√°rio: ");
 		salario = input.nextDouble();
 		
 		if(salario <= 300) {
-			System.out.println("Sal·rio reajustado: R$ "+(salario * 1.35));
+			System.out.println("Sal√°rio reajustado: R$ "+(salario * 1.35));
 		}
 		else {
-			System.out.println("Sal·rio reajustado: "+(salario * 1.15));
+			System.out.println("Sal√°rio reajustado: "+(salario * 1.15));
 		}
 		
 		input.close();
